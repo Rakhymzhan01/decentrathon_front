0ec02e74065ec58914dee65e7806a0e9b038bffc
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-10-20 00:08:45 +0500

    centred landing

--- src/components/financial-literacy-landing.tsx
+++ src/components/financial-literacy-landing.tsx
@@ -179,9 +179,9 @@ export function FinancialLiteracyLanding() {
         </div>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 flex justify-center items-center text-center">
           <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
+            <div className="flex flex-col items-center space-y-4">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                   Master Your Finances with FinLit Academy
@@ -192,11 +192,9 @@ export function FinancialLiteracyLanding() {
               </div>
               <div className="space-x-4">
                 <Dialog>
-                  {/* <DialogTrigger asChild> */}
-                    <Button>
-                      <Link href={"/survey"}>Get Started</Link>
-                    </Button>
-                  {/* </DialogTrigger> */}
+                  <DialogTrigger asChild>
+                    <Button>Get Started</Button>
+                  </DialogTrigger>
                   <DialogContent>
                     <DialogHeader>
                       <DialogTitle>Get Started with FinLit Academy</DialogTitle>
