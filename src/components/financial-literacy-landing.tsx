fe9735c04c1698de8f578753349a7f874666ac1c
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-10-20 00:19:22 +0500

    auth fix

--- src/components/financial-literacy-landing.tsx
+++ src/components/financial-literacy-landing.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { use, useState } from "react"
+import { useState } from "react"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
